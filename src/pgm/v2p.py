448f95175a3c52759b484fd4ebdd877b260cccb6
underhill1886 <elviswang95@gmail.com>
2020-07-10 15:58:49 -0400

    Modify v2p function

--- src/pgm/v2p.py
+++ src/pgm/v2p.py
@@ -5,20 +5,20 @@
    :synopsis:
 .. moduleauthor:: Tian Qin <qinxx197@umn.edu>
 .. moduleauthor:: Qi Zhang <qz2280@columbia.edu>
+.. moduleauthor:: Hongjin Wang <hw2626@columbia.edu>
 """
 
 # import numba
 import numpy as np
 
 from pgm.util.tools import find_value, vectorized_find_nearest
-# from qha.type_aliases import Matrix, Vector
 
 # ===================== What can be exported? =====================
 __all__ = ['v2p']
 
 
 # @numba.jit(nopython=True, parallel=True)
-def _lagrange4(x: float, x0, x1, x2, x3, y0, y1, y2, y3) -> float:
+def lagrange4(x: float, x0, x1, x2, x3, y0, y1, y2, y3) -> float:
     """
     A third-order Lagrange polynomial function. Given 4 points for interpolation:
     :math:`(x_0, y_0), \\ldots, (x_3, y_3)`, evaluate the Lagrange polynomial on :math:`x`.
@@ -31,14 +31,14 @@ def _lagrange4(x: float, x0, x1, x2, x3, y0, y1, y2, y3) -> float:
     :param y0: The y-coordinate of the 1st point.
     :param y1: The y-coordinate of the 2nd point.
     :param y2: The y-coordinate of the 3rd point.
-    :param y3: The y-coordinate of the 4th point.
+    :param y3: The y-coordinate of the 4th po`int.
     :return: The y-coordinate of the point to be evaluated.
     """
     return (x - x1) * (x - x2) * (x - x3) / (x0 - x1) / (x0 - x2) / (x0 - x3) * y0 + \
            (x - x0) * (x - x2) * (x - x3) / (x1 - x0) / (x1 - x2) / (x1 - x3) * y1 + \
            (x - x0) * (x - x1) * (x - x3) / (x2 - x0) / (x2 - x1) / (x2 - x3) * y2 + \
            (x - x0) * (x - x1) * (x - x2) / \
-        (x3 - x0) / (x3 - x1) / (x3 - x2) * y3
+           (x3 - x0) / (x3 - x1) / (x3 - x2) * y3
 
 
 def v2p(func_of_t_v, p_of_t_v, desired_pressures):
@@ -52,27 +52,39 @@ def v2p(func_of_t_v, p_of_t_v, desired_pressures):
     :param desired_pressures: A vector of pressures which user wants to apply.
     :return: The interpolated function :math:`f` on :math:`(T, P)` grid.
     """
+    # Assume the function is temperature in row and volume in column
     t_amount, v_amount = func_of_t_v.shape
+
     desired_pressures_amount = len(desired_pressures)
 
     result = np.empty((t_amount, desired_pressures_amount))
 
+    # Put three parts together
+    # third column, function tv, last fourth column
+    # TODO: Still confused of the artificial choice of 3 and -4. Why? Can't I just use 0 and -1 lmao?
+    # Made a little test, feel like 0 and -1 is fine by me. Stick to 0 -1 for now.
     extended_f = np.hstack(
-        (func_of_t_v[:, 3].reshape(-1, 1), func_of_t_v, func_of_t_v[:, -4].reshape(-1, 1)))
+        (func_of_t_v[:, 0].reshape(-1, 1), func_of_t_v, func_of_t_v[:, -1].reshape(-1, 1)))
     extended_p = np.hstack(
-        (p_of_t_v[:, 3].reshape(-1, 1), p_of_t_v, p_of_t_v[:, -4].reshape(-1, 1)))
+        (p_of_t_v[:, 0].reshape(-1, 1), p_of_t_v, p_of_t_v[:, -1].reshape(-1, 1)))
 
+    # Proceed row by row
     for i in range(t_amount):
         rs = np.zeros(desired_pressures_amount)
-        #TODO:change the function
-        vectorized_find_nearest(extended_p[i], desired_pressures, rs)
+
+        # Search desired pressures in the extended p array
+        if (np.min(desired_pressures) > np.max(extended_p[i])) or (np.max(desired_pressures) < np.min(extended_p[i])):
+            raise ValueError("Desired pressure is out of bound. Try to change the volume expansion ratio.")
+        else:
+            vectorized_find_nearest(extended_p[i], desired_pressures, rs)
 
         for j in range(desired_pressures_amount):
+            # The index of found desired pressure in the extended p
             k = int(rs[j])
 
-            x1, x2, x3, x4 = extended_p[i, k - 1:k + 3]
-            f1, f2, f3, f4 = extended_f[i, k - 1:k + 3]
+            x1, x2, x3, x4 = extended_p[i, k: k + 4]
+            f1, f2, f3, f4 = extended_f[i, k: k + 4]
 
-            result[i, j] = _lagrange4(
+            result[i, j] = lagrange4(
                 desired_pressures[j], x1, x2, x3, x4, f1, f2, f3, f4)
     return result
