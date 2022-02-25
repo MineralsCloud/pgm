279b4326283896e63c27c42420a41385f4c9778a
underhill1886 <elviswang95@gmail.com>
2022-02-25 13:49:06 -0500

    refractor: remove unused code, merge zp and qha functions

--- src/pgm/calculator.py
+++ src/pgm/calculator.py
@@ -55,19 +55,18 @@ class FreeEnergyCalculation:
         # calculate vibrational free energies using pgm
         vib_energies, vib_entropies = pgm(self.NV, self.ratio, self.continuous_temperature, calc_input)
 
-        volumes, zp_energies, _ = zp(self.NV, self.ratio, calc_input)
+        volumes, zp_energies, static_energies = zp_and_static(self.NV, self.ratio, calc_input)
 
-        volumes, energies, static_energies = qha(self.NV, self.ratio, calc_input)
-
+        # volumes, energies, static_energies = static_energy(self.NV, self.ratio, calc_input)
 
         # interpolation between configurations(temperatures)
         static_free_energies, interpolated_volumes = spline_interpolation(volumes, static_energies,
                                                                           self.discrete_temperatures,
                                                                           self.continuous_temperature)
         zp_free_energies, _ = spline_interpolation(volumes, zp_energies,
-                                                                          self.discrete_temperatures,
-                                                                          self.continuous_temperature)
-        total_free_energies = vib_energies + static_free_energies 
+                                                   self.discrete_temperatures,
+                                                   self.continuous_temperature)
+        total_free_energies = vib_energies + static_free_energies
 
         # if the first temperature isn't 0, the free energy needs to minus a base energy
         if 0 not in self.discrete_temperatures:
@@ -123,12 +122,12 @@ def pgm(NTV, ratio, continuous_temperatures, input: Input = None):
     return vib_free_energies, interpolated_entropies
 
 
-def zp(NTV, ratio, input: Input):
+def zp_and_static(NTV, ratio, input: Input):
     """
     Computes zero point energy
     return:
     all_volumes: interpolated volumes at different discrete temperatures
-    all_energies: interpolated qha energies at different discrete temperatures
+    all_zp_energies: interpolated qha energies at different discrete temperatures
     all_static_energies: interpolated static energies at different discrete temperatures
     """
 
@@ -138,7 +137,7 @@ def zp(NTV, ratio, input: Input):
         """
 
         @jit(nopython=True, parallel=True)
-        def zp_vib_energy(temperature, frequency):
+        def zp_formula(temperature, frequency):
             kt = K * temperature
             shape = frequency.shape
             frequency = frequency.reshape(-1)
@@ -150,13 +149,13 @@ def zp(NTV, ratio, input: Input):
             return result.reshape(shape)
 
         scaled_q_weights = weights / np.sum(weights)
-        zp_energies = np.dot(np.nan_to_num(zp_vib_energy(
+        zp_energies = np.dot(np.nan_to_num(zp_formula(
             temperature, frequency)).sum(axis=2), scaled_q_weights)
 
         return zp_energies
 
     all_volumes = []
-    all_energies = []
+    all_zp_energies = []
     all_static_energies = []
     input_dict = input.get_input()
 
@@ -175,73 +174,13 @@ def zp(NTV, ratio, input: Input):
         new_vib_f = inter.fitting(vib_f)
 
         all_volumes.append(inter.out_volumes)
-        all_energies.append(new_vib_f)
+        all_zp_energies.append(new_vib_f)
         all_static_energies.append(static_f)
 
     all_volumes = np.array(all_volumes)
-    all_zp_energies = np.array(all_energies)
+    all_zp_energies = np.array(all_zp_energies)
     all_static_energies = np.array(all_static_energies)
     return all_volumes, all_zp_energies, all_static_energies
-
-
-def qha(NTV, ratio, input: Input):
-    """
-    Computes qha energy
-    return:
-    all_volumes: interpolated volumes at different discrete temperatures
-    all_energies: interpolated qha energies at different discrete temperatures
-    all_static_energies: interpolated static energies at different discrete temperatures
-    """
-
-    def qha_energy(temperature, frequency, weights):
-        """
-        Compute vibrational free energies using qha formula
-        """
-
-        @jit(nopython=True, parallel=True)
-        def vib_energy(temperature, frequency):
-            kt = K * temperature
-            shape = frequency.shape
-            frequency = frequency.reshape(-1)
-
-            # if frequency is negative, they are treated as 0
-            frequency[frequency < 0] = 0.0
-            hw = HBAR * frequency
-            result = 1 / 2 * hw + kt * np.log(1 - np.exp(-hw / kt))
-            return result.reshape(shape)
-
-        scaled_q_weights = weights / np.sum(weights)
-        vibrational_energies = np.dot(np.nan_to_num(vib_energy(temperature, frequency)).sum(axis=2), scaled_q_weights)
-
-        return vibrational_energies
-
-    all_volumes = []
-    all_energies = []
-    all_static_energies = []
-    input_dict = input.get_input()
-
-    for temp in input_dict.keys():
-        vib_f = qha_energy(temp, input_dict[temp][3], input_dict[temp][
-            4])  # compute vibrational free energies using qha formula, only vibrational!
-
-        # input_dict[temp][1] is the volumes from a single qha input file
-        # inter is used to calculate the strain based on the volume
-        inter = Interpolation(input_dict[temp][1], NTV, ratio)
-
-        # input_dict[temp][2] is the static free energy from a single qha input file
-        # Then interpolate between the static free energy using the strain calculated in inter
-        static_f = inter.fitting(input_dict[temp][2])
-
-        new_vib_f = inter.fitting(vib_f)
-
-        all_volumes.append(inter.out_volumes)
-        all_energies.append(new_vib_f)
-        all_static_energies.append(static_f)
-
-    all_volumes = np.array(all_volumes)
-    all_energies = np.array(all_energies)
-    all_static_energies = np.array(all_static_energies)
-    return all_volumes, all_energies, all_static_energies
 
 
 def fit_entropy(raw_volumes, raw_entropy, discrete_temperatures, continuous_temperatures
@@ -280,33 +219,11 @@ def fit_entropy(raw_volumes, raw_entropy, discrete_temperatures, continuous_temp
 
     def fit_freq(t, s, tnew):
         """
-        Fitting function for  ...
+        Fitting function for entropy
         return the entropy in a finer T grid by interpolating the 'efficient' frequency
         - important: not working!
         """
-        # freq_eff = []
-        # print("start")
-        # for i in range(len(t)):
-        #     si = s[i]
-        #     ti = t[i]
-        #     a = symbols('a', real=True, positive=True)
-        #
-        #     # a = symbols('a')
-        #     # eq = round(K*10**7, 3) * (a / tanh(a) - log(2 * sinh(a))) - round(si*10**7, 3)
-        #     eq = round(K*10**7, 3) * ((1+a)*log(1+a) - a * log(a)) - round(si*10**7, 3)
-        #     print(eq)
-        #     solutions = solve(eq, quick=True)
-        #     print(solutions)
-        #     # onlysolution = solutions[0]
-        #     # hw_2kt_eff = onlysolution.evalf(subs=onlysolution)
-        #     # kt =  K * ti
-        #     # hw_eff = hw_2kt_eff * (2*kt)
-        #     # w_eff = hw_eff/HBAR
-        #     # freq_eff.append(w_eff)
-        #
-        # print(freq_eff)
-
-        return None
+        raise NotImplementedError
 
     configurations_amount, volume_number = raw_volumes.shape
     index = 0
@@ -430,26 +347,6 @@ def entropy(temperature, frequency, weights):
     vibrational_entropies = np.dot(np.nan_to_num(vib_entropy(temperature, frequency)).sum(axis=2), scaled_q_weights)
 
     return vibrational_entropies
-
-
-def zero_point_motion(temperature, frequency, weights):
-    def vib_entropy(temperature, frequency):
-        kt = K * temperature
-        mat = np.zeros(frequency.shape)
-        for i in range(frequency.shape[0]):
-            for j in range(frequency.shape[1]):
-                for k in range(frequency.shape[2]):
-                    if frequency[i][j][k] <= 0:
-                        mat[i][j][k] = 0
-                    else:
-                        freq = frequency[i][j][k]
-                        hw_2 = HBAR * freq / 2
-                        mat[i][j][k] = hw_2
-        return mat
-
-    scaled_q_weights = weights / np.sum(weights)
-    zero_free_energies = np.dot(vib_entropy(temperature, frequency).sum(axis=2), scaled_q_weights)
-    return zero_free_energies
 
 
 def intergrate(temperatures, entropies):
