2d53eeda4f301489dd9d423be3a61bf4210c9bb9
underhill1886 <elviswang95@gmail.com>
2022-03-07 14:11:42 -0500

    fix: fix the unclean setup route

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-    Setup file for pgm.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 3.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
 import sys
 from pathlib import Path
 from pkg_resources import require, VersionConflict
@@ -33,12 +25,10 @@ setup(
     author_email='hw2626@columbia.edu',
     url='https://github.com/MineralsCloud/pgm/',
     packages=find_packages('src'),
+    package_dir={'':'src'},
     install_requires=[
-        "bigfloat",
         "cycler",
-        "kiwisolver",
         "lazy-property",
-        "llvmlite",
         "numba",
         "numpy",
         "pandas",
@@ -48,7 +38,6 @@ setup(
         "PyYAML",
         "scientific-string",
         "scipy",
-        "seaborn",
         "six",
         "text-stream",
     ],
@@ -61,4 +50,4 @@ setup(
 )
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    pass
