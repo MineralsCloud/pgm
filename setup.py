43662116ea432ffc994008ec9fa89ae1c7365eae
underhill1886 <elviswang95@gmail.com>
2022-03-07 12:45:11 -0500

    feat: Using click package to manage cli

--- setup.py
+++ setup.py
@@ -9,27 +9,56 @@
     Learn more under: https://pyscaffold.org/
 """
 import sys
-
+from pathlib import Path
 from pkg_resources import require, VersionConflict
 from setuptools import setup, find_packages
+
+with open(Path(__file__).parent / "src" / "pgm" / "version.py") as fp: exec(fp.read())
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 try:
     require('setuptools>=38.3')
 except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
-setup(name='pgm',
-      version='0.0.2',
-      packages=[
-          'pgm',
-          'pgm.reader',
-          'pgm.cli',
-          'pgm.util',
-      ],
-      entry_points={
-          'console_scripts': [
-              'pgm=pgm.cli:main'
-          ], })
+
+setup(
+    name='pgm',
+    version=__version__,
+    description='Free energy calculation using phonon gas model',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Hongjin Wang',
+    author_email='hw2626@columbia.edu',
+    url='https://github.com/MineralsCloud/pgm/',
+    packages=find_packages('src'),
+    install_requires=[
+        "bigfloat",
+        "cycler",
+        "kiwisolver",
+        "lazy-property",
+        "llvmlite",
+        "numba",
+        "numpy",
+        "pandas",
+        "pyparsing",
+        "python-dateutil",
+        "pytz",
+        "PyYAML",
+        "scientific-string",
+        "scipy",
+        "seaborn",
+        "six",
+        "text-stream",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pgm=pgm.cli.cli:main',
+            'pgm-run=pgm.cli.main:main',
+        ],
+    }
+)
 
 if __name__ == "__main__":
     setup(use_pyscaffold=True)
