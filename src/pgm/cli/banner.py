8250c78575ae3b3838b1c14af98b0860808979e3
Hongjin Wang <hw2626@columbia.edu>
2023-03-19 19:15:47 -0400

    Update version

--- src/pgm/cli/banner.py
+++ src/pgm/cli/banner.py
@@ -1,13 +1,13 @@
+from pgm.version import __version__
 def print_banner():
-    print("""
-                            
+    print(f"""
   _ __   __ _ _ __ ___  
  | '_ \ / _` | '_ ` _ \ 
  | |_) | (_| | | | | | |
  | .__/ \__, |_| |_| |_|
  | |     __/ |          
  |_|    |___/           
- Welcome to ****!
+ Version {__version__}
     """)
 
 if __name__=="__main__":
