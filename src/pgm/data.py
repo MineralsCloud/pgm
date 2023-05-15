c040b7a31416a9fad26b2d310e2c900c0d170efa
Hongjin Wang <hw2626@columbia.edu>
2023-05-15 18:01:16 -0400

    Fix read_data, remove unused code

--- src/pgm/data.py
+++ src/pgm/data.py
@@ -1,29 +1,14 @@
 import pandas as pd
 import numpy as np
 import gc
-# from geothermpy import bind, Point
-from scipy.optimize import curve_fit
-
-
-def read_tp(filename='TP'):
-    return pd.read_csv(filename, delim_whitespace=True, names=['T', 'P', 'dT/dP'])
 
 
 def read_data(filename: str, index_col) -> pd.DataFrame:
     """
     read from a csv file, basicly qha output style
     """
-    return pd.read_csv(filename, delim_whitespace=True, index_col=index_col)
+    return pd.read_table(filename, sep=',', header=0, index_col=0)
 
-
-# def generate_adiabatic_tp(geotherm, index, columns, init_p = 55, init_t = 1717):
-#     """
-#     generate adiabatic temperature vs pressure from a geotherm matrix
-#     """
-#     gg = pd.DataFrame(geotherm, index = index, columns =columns)
-#     trace = bind.generate_trace(gg, Point(init_p, init_t), h=0.02, n=100000)
-#     xs, ys = np.array([p.x for p in trace]), np.array([p.y for p in trace])
-#     return xs, ys
 
 def save_data(quantities, index, column, filename):
     """
